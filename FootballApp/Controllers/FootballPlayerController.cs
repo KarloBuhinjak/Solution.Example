4151000e2f4fbf4c9b6f8968c8701f64a2750085
Karlo Buhinjak <buhinjak.k@gmail.com>
2024-05-29 11:25:05 +0200

    created FootballPlayer and FootballPlayerController classes

---
+++ FootballApp/Controllers/FootballPlayerController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FootballPlayerController : ControllerBase
+    {
+        private static readonly List<FootballPlayer> Players = new List<FootballPlayer>
+        {
+            new FootballPlayer { Id = 1, Name = "Messi", Age = 34, Position = "Forward", Club = "FC Barcelona" },
+            new FootballPlayer { Id = 2, Name = "Ronaldo", Age = 36, Position = "Forward", Club = "Manchester United" }
+        };
+        
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return Players;
+        }
+        
+        [HttpPost]
+        public ActionResult<FootballPlayer> Post(FootballPlayer player)
+        {
+            player.Id = Players.Count + 1; // Automatski dodijeli ID novom igraču
+            Players.Add(player);
+            return CreatedAtAction(nameof(Get), new { id = player.Id }, player);
+        }
+        
+    }
+}
